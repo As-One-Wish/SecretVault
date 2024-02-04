448f70d3caec7d8e26834c1cf42f657e5b6c5363
HWJ <hwj20010504@163.com>
2024-02-04 18:17:08 +0800

    完成用户数据库实体

---
+++ Info.Storage.WebApi/Infrastructure/Info.Store.Infa.Entity/ModuleUserManagement/Enums/UserEnums.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Info.Storage.Infa.Entity.ModuleUserManagement.Enums
+{
+    /// <summary>
+    /// 性别枚举
+    /// </summary>
+    public enum GenderEnum
+    {
+        [Description("未知")]
+        U = 0,
+
+        [Description("男")]
+        M = 1,
+
+        [Description("女")]
+        F = 2
+    }
+}
---
+++ Info.Storage.WebApi/Infrastructure/Info.Store.Infa.Repository/Databases/Entities/AppUser.cs
@@ -0,0 +1,67 @@
+﻿using FreeSql.DataAnnotations;
+using Info.Storage.Infa.Entity.ModuleUserManagement.Enums;
+using Newtonsoft.Json;
+
+namespace Info.Storage.Infa.Repository.Databases.Entities
+{
+    /// <summary>
+    /// 用户信息表
+    /// </summary>
+    [JsonObject(MemberSerialization.OptIn), Table(Name = "user")]  // MemberSerialization.OptIn -> 表示只有被显式标记为'JsonProperty'的成员才会被包括在序列化中
+    public class AppUser
+    {
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        [JsonProperty, Column(Name = "user_id", IsPrimary = true)]
+        public long UserId { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        [JsonProperty, Column(Name = "user_name", DbType = "varchar(60)")]
+        public string UserName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 用户账号
+        /// </summary>
+        [JsonProperty, Column(Name = "user_account", DbType = "varchar(60)")]
+        public string UserAccount { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 用户密码
+        /// </summary>
+        [JsonProperty, Column(Name = "user_pwd", DbType = "varchar(60)")]
+        public string UserPwd { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 用户电话
+        /// </summary>
+        [JsonProperty, Column(Name = "user_phone", DbType = "varchar(60)")]
+        public string UserPhone { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 用户头像
+        /// </summary>
+        [JsonProperty, Column(Name = "user_avatar", DbType = "text")]
+        public string UserAvatar { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 用户性别
+        /// </summary>
+        [JsonProperty, Column(Name = "user_gender", MapType = typeof(short))]
+        public GenderEnum UserGender { get; set; } = GenderEnum.U;
+
+        /// <summary>
+        /// 是否删除标记
+        /// </summary>
+        [JsonProperty, Column(Name = "deleted")]
+        public bool IsDeleted { get; set; }
+
+        /// <summary>
+        /// 更新时间
+        /// </summary>
+        [JsonProperty, Column(Name = "update_time", ServerTime = DateTimeKind.Local)] // 写入数据库时使用本地时间
+        public DateTime? UpdateTime { get; set; }
+    }
+}
