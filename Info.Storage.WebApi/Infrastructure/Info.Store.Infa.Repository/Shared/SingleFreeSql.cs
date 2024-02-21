aaafce39b9ddade7aafabb691e5cc8ef26d4008b
HWJ <hwj20010504@163.com>
2024-02-21 11:20:33 +0800

    修改FreeSql配置方式

---
+++ Info.Storage.WebApi/Infrastructure/Info.Store.Infa.Repository/Shared/SingleFreeSql.cs
@@ -0,0 +1,44 @@
+﻿using Info.Storage.Infa.Entity.Shared.Settings;
+using Info.Storage.Utils.CommonHelper.Helpers;
+using Microsoft.Extensions.Configuration;
+
+namespace Info.Storage.Infa.Repository.Shared
+{
+    public class SingleFreeSql
+    {
+        private IFreeSql _freeSql;
+
+        /// <summary>
+        /// 注册FreeSql
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public IFreeSql RegisterFreeSql(IConfiguration configuration)
+        {
+            DbConnectionOptionConfig oFreeSqlDbConnectionItemConfig = configuration.GetSection("DbConnectionStrings:DbAppPostgresqlConnectionString").Get<DbConnectionOptionConfig>();
+            if (oFreeSqlDbConnectionItemConfig?.MasterConnection != null)
+            {
+                var freeBuilder = new FreeSql.FreeSqlBuilder()
+                .UseConnectionString(FreeSql.DataType.PostgreSQL, oFreeSqlDbConnectionItemConfig.MasterConnection)
+                .UseAutoSyncStructure(false) // 关闭自动同步结构
+                .UseAdoConnectionPool(true) // 启用连接池
+                .UseGenerateCommandParameterWithLambda(true) // 启用基于Lambda表达式的命令参数生成
+                .UseLazyLoading(true) // 启用延迟加载
+                .UseMonitorCommand(cmd => { LogHelper.Info(cmd.CommandText); }); // 监控SQL命令执行，并记录信息
+
+                // 是否开启读写分离配置
+                if (oFreeSqlDbConnectionItemConfig.SlaveConnections.Count > 0)
+                    freeBuilder.UseSlave(oFreeSqlDbConnectionItemConfig.SlaveConnections.ToArray());
+
+                _freeSql = freeBuilder.Build();
+                // 语句执行前对所执行的Sql语句进行操作
+                _freeSql.Aop.CurdBefore += (s, e) =>
+                {
+                    string strSql = e.Sql;
+                };
+                return _freeSql;
+            }
+            return null;
+        }
+    }
+}
--- Info.Storage.WebApi/Presentation/Info.Storage.HttpApi.Host/Configurations/FreeSqlConfig.cs
+++ Info.Storage.WebApi/Presentation/Info.Storage.HttpApi.Host/Configurations/FreeSqlConfig.cs
@@ -1,4 +1,5 @@
 ﻿using Info.Storage.Infa.Entity.Shared.Settings;
+using Info.Storage.Infa.Repository.Shared;
 using Info.Storage.Utils.CommonHelper.Helpers;
 
 namespace Info.Storage.HttpApi.Host.Configurations
@@ -16,33 +17,8 @@ namespace Info.Storage.HttpApi.Host.Configurations
         public static void AddFreeSqlConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
             if (services == null) throw new ArgumentNullException(nameof(services));
-
-            DbConnectionOptionConfig? oFreeSqlDbConnectionItemConfig = configuration.GetSection("DbConnectionStrings:DbAppPostgresqlConnectionString").Get<DbConnectionOptionConfig>();
-            IFreeSql? freeSql = null;
-            if (oFreeSqlDbConnectionItemConfig?.MasterConnection != null)
-            {
-                var freeBuilder = new FreeSql.FreeSqlBuilder()
-                .UseConnectionString(FreeSql.DataType.PostgreSQL, oFreeSqlDbConnectionItemConfig.MasterConnection)
-                .UseAutoSyncStructure(false) // 关闭自动同步结构
-                .UseAdoConnectionPool(true) // 启用连接池
-                .UseGenerateCommandParameterWithLambda(true) // 启用基于Lambda表达式的命令参数生成
-                .UseLazyLoading(true) // 启用延迟加载
-                .UseMonitorCommand(cmd => { LogHelper.Info(cmd.CommandText); }); // 监控SQL命令执行，并记录信息
-
-                // 是否开启读写分离配置
-                if (oFreeSqlDbConnectionItemConfig.SlaveConnections.Count > 0)
-                    freeBuilder.UseSlave(oFreeSqlDbConnectionItemConfig.SlaveConnections.ToArray());
-
-                freeSql = freeBuilder.Build();
-                // 语句执行前对所执行的Sql语句进行操作
-                freeSql.Aop.CurdBefore += (s, e) =>
-                {
-                    string strSql = e.Sql;
-                };
-            }
-
-            if (freeSql != null)
-                services.AddSingleton(freeSql);
+            var singleFreeSql = new SingleFreeSql();
+            services.AddSingleton(singleFreeSql.RegisterFreeSql(configuration));
         }
     }
 }
