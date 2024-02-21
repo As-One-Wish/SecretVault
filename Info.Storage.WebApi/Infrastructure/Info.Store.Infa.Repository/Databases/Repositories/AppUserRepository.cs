689cdc5a8b5b0fb25c553187c63fc18da7492246
HWJ <hwj20010504@163.com>
2024-02-21 10:55:49 +0800

    新增用户仓储相关

---
+++ Info.Storage.WebApi/Infrastructure/Info.Store.Infa.Repository/Databases/Repositories/AppUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Info.Storage.Infa.Repository.Databases.Entities;
+using Info.Storage.Infa.Repository.Extension;
+
+namespace Info.Storage.Infa.Repository.Databases.Repositories
+{
+    public class AppUserRepository : BaseRepositoryExtend<AppUser, long>
+    {
+        public AppUserRepository(IFreeSql fsql) : base(fsql, null, null)
+        {
+        }
+    }
+}
